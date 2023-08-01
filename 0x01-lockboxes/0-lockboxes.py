e6b1cdecf5195c75fd770fef21d544ed56d4ca84
Njaaga-Gakure <njaagagakure@gmail.com>
2023-08-01 10:57:17 +0000

    Refactor code because of time complexity of nested loops

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,19 +13,17 @@ def canUnlockAll(boxes):
         True if all boxes can be opened, false otherwise
 
     """
+    if not any(isinstance(box, list) for box in boxes):
+        return False
     if len(boxes) == 1:
         return True
-    if not all(isinstance(box, list) for box in boxes):
-        return False
-    is_open = []
-    for i in range(len(boxes)):
-        for j in range(len(boxes[i])):
-            for k in range(1, len(boxes)):
-                if i == k:
-                    continue
-                else:
-                    if boxes[i][j] == k:
-                        is_open.append("{} is_opened".format(k))
-    unique_list = []
-    [unique_list.append(item) for item in is_open if item not in unique_list]
-    return True if len(unique_list) == (len(boxes) - 1) else False
+    opened_boxes = [0]
+    available_keys = boxes[0].copy()
+    while available_keys:
+        key = available_keys.pop()
+        if key in opened_boxes:
+            continue
+        if key < len(boxes):
+            opened_boxes.append(key)
+            available_keys.extend(boxes[key])
+    return len(opened_boxes) == len(boxes)
